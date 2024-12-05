5c0ad337f193e81aee04dca7b30e3c377c834642
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-12-04 23:53:31 -0300

    Update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (!request.nextUrl.search) {
-    return NextResponse.next();
-  }
+  if (!request.nextUrl.search) return NextResponse.next();
   const searchParams = new URLSearchParams(request.nextUrl.search);
   const slug = searchParams.get("slug");
+  if (!slug) return NextResponse.next();
   const slot = searchParams.get("slot");
-  if (!slug) {
-    return NextResponse.next();
-  }
-  return NextResponse.redirect(
-    new URL(`/cardapio/${slug}?slot=${slot}`, request.url)
-  );
+  const url = `/cardapio/${slug}?slot=${slot}`;
+  return NextResponse.redirect(new URL(url, request.url));
 }
 
 export const config = {
